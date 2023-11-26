6f4ca0a31c854307d6da3bfe29d83de72f54f79d
saipepu <saipepu.mdy257@gmail.com>
2023-11-26 14:57:33 +0700

    navlink &active link effect

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,10 +1,8 @@
-import Navbar from '@/app/components/Navbar'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="font-Poppins h-full w-full bg-[#2d2d2d]">
-      <Navbar />
       Home
     </main>
   )
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,20 @@
+"use client";
+
 import Link from 'next/link'
+import { useParams, usePathname } from 'next/navigation'
 import React from 'react'
 
 const Navbar = () => {
+
+  const params = usePathname()
+  console.log(params)
+
   return (
     <div className="bg-[#2d2d2d] w-full py-[24px] px-[50px] flex flex-row justify-between items-center">
       <h1>SAI PEPU</h1>
       <div className="flex flex-row justify-center items-center gap-[10px] md:gap-[20px]">
-        <Link href="/">projects</Link>
-        <Link href="/">bio</Link>
+        <Link href="/projects" className={params === '/projects' ? 'text-[#666666]' : 'block'}>projects</Link>
+        <Link href="/bio" className={params === '/bio' ? 'text-[#666666]' : 'block'}>bio</Link>
       </div>
     </div>
   )
