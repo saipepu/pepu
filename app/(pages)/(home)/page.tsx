36d85025a1e6c5d294f2a8ed1a5ba42ea9dd17f8
saipepu <saipepu.mdy257@gmail.com>
2023-12-08 16:48:18 +0700

    home page init

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,9 +1,21 @@
-import Image from 'next/image'
+import React from 'react'
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="font-Poppins h-full w-full bg-[#2d2d2d]">
-      Home
+    <main className="h-full bg-[#2d2d2d]">
+      <div className="h-full w-full pt-[90px]">
+        <div className="h-full w-full flex flex-col justify-start items-center px-[16px] md:px-[50px]">
+          <div className="h-full w-full flex flex-col justify-end items-start bg-gray-300 rounded-[45px] overflow-hidden p-[15px]">
+            <p className="w-full text-[52px]">I am PePu</p>
+            <p>Keep in Touch!</p>
+          </div>
+          <div className="h-full w-full flex flex-col justify-end items-start">
+            Hello
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
+
+export default Home
